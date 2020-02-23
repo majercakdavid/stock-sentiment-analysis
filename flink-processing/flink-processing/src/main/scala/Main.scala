3bea6a42c932d32671029ed8bb60fc4573918709
viktorria <viktoria.simsikova@outlook.com>
2020-02-23 21:39:55 +0100

    update pipeline

--- flink-processing/flink-processing/src/main/scala/Main.scala
+++ flink-processing/flink-processing/src/main/scala/Main.scala
@@ -1,16 +1,18 @@
-import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.scala.{
+  DataStream,
+  StreamExecutionEnvironment
+}
 import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows
 import org.apache.flink.streaming.api.watermark.Watermark
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.scala._
 
 import spray.json._
 import DefaultJsonProtocol._
-import scala.math.min
 
 case class TweetText(val id: Long, val text: String)
 case class TweetSentiment(val id: Long, val sentiment: Double)
-case class StockQuote(val symbol: String, val price: Double, val timestamp: Long, val count: Long)
 
 object FlinkProcessingJsonProtocol extends DefaultJsonProtocol {
   implicit val tweetTextFormat = jsonFormat2(TweetText)
@@ -23,72 +25,23 @@ object Main {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.getConfig.disableClosureCleaner()
 
-    val kafkaTweetsConsumer = new KafkaFlinkConsumer("tweets")
-    val kafkaTweetsTextProducer = new KafkaFlinkProducer("tweets-text")
-
-    // val tweetEvents = env
-    //   .addSource(kafkaTweetsConsumer.consumer)
-    //   .map{msg => {
-    //     msg.parseJson
-    //       .asJsObject.getFields("id", "text") match {
-    //         case Seq(JsNumber(id), JsString(text)) => {
-    //           val tweetText = TweetText(id.toLong, text)
-    //           tweetText.toJson.compactPrint
-    //         }
-    //         case _ => throw new DeserializationException("Tweet text expected")
-    //       }
-    //   }}
-    //   .addSink(kafkaTweetsTextProducer.producer)
-
-    // val kafkaTweetsSentimentConsumer = new KafkaFlinkConsumer("tweets-sentiment")
-    // val tweetSentimentEvents = env
-    //   .addSource(kafkaTweetsSentimentConsumer.consumer)
-    //   .map{msg => {
-    //     msg.parseJson
-    //       .asJsObject.getFields("id", "sentiment") match {
-    //         case Seq(JsNumber(id), JsNumber(sentiment)) => TweetSentiment(id.toLong, sentiment.toDouble)
-    //         case _ => throw new DeserializationException("Tweet text expected")
-    //       }
-    //   }}
-    //   .addSink(print(_))
-
     val kafkaStockQuotesConsumer = new KafkaFlinkConsumer("stock-quotes")
-    val stockQuotesEvents = env
-      .addSource(kafkaStockQuotesConsumer.consumer)
-      .map{msg => {
-        msg.parseJson
-          .asJsObject.getFields("symbol", "price", "timestamp") match {
-            case Seq(JsString(symbol), JsNumber(price), JsNumber(timestamp)) => StockQuote(symbol, price.toDouble, timestamp.toLong, 1)
-            case _ => throw new DeserializationException("Stock quote deserializatiom error")
-          }
-      }}
-      .addSink(print(_))
-
     val stockQuotesAggregatedEvents = env
       .addSource(kafkaStockQuotesConsumer.consumer)
-      .map{msg => {
-        msg.parseJson
-          .asJsObject.getFields("symbol", "price", "timestamp") match {
-            case Seq(JsString(symbol), JsNumber(price), JsNumber(timestamp)) => StockQuote(symbol, price.toDouble, timestamp.toLong, 1)
-            case _ => throw new DeserializationException("Stock quote deserializatiom error")
-          }
-      }}
-      .keyBy(_.symbol)
-      .assignTimestampsAndWatermarks(
-        new AssignerWithPunctuatedWatermarks[StockQuote] {
-          override def extractTimestamp(stockQuote: StockQuote, previousStockQuoteTimestamp: Long): Long = stockQuote.timestamp
-          override def checkAndGetNextWatermark(lastStockQuote: StockQuote, extractedTimestamp: Long): Watermark = {
-            new Watermark(extractedTimestamp - 2)
-          }
-        })
-      .timeWindowAll(Time.seconds(60))
-      .reduce((sq1, sq2) => StockQuote(
-        sq1.symbol,
-        sq1.price + sq2.price,
-        min(sq1.timestamp, sq2.timestamp),
-        sq1.count + sq2.count
-      ))
-      .addSink(print(_))
+
+    val kafkaStockQuotesFastMAProducer = new KafkaFlinkProducer(
+      "stock-quotes-ma-5-1"
+    )
+    ProcessStockQuotes
+      .process(stockQuotesAggregatedEvents, 5, 1)
+      .addSink(kafkaStockQuotesFastMAProducer.producer)
+
+    val kafkaStockQuotesSlowMAProducer = new KafkaFlinkProducer(
+      "stock-quotes-ma-15-1"
+    )
+    ProcessStockQuotes
+      .process(stockQuotesAggregatedEvents, 15, 1)
+      .addSink(kafkaStockQuotesSlowMAProducer.producer)
 
     env.execute()
   }
